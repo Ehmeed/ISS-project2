c2ada1366e2038fab4211442c243dead771ba086
David Hěl <dave.hel7@gmail.com>
2017-10-01 01:55:33 +0200

    nastylovane tabulky

--- all_subjects.php
+++ all_subjects.php
@@ -1,24 +1,40 @@
 <?php
-    session_start();
-    require_once 'check_login.php';
-    require_once 'dbconnect.php';
-    $title = 'Všechny předměty - Fakultní informační systém';
-    include("template/header.php"); 
+session_start();
+require_once 'check_login.php';
+require_once 'dbconnect.php';
+$title = 'Všechny předměty - Fakultní informační systém';
+include("template/header.php");
 ?>
 
             <h2>Všechny předměty</h2>
-            <?php
-            $query = "SELECT * FROM predmet, vyucujici WHERE predmet.id_garant = vyucujici.id_vyucujici";
-            $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
-            while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
-                echo "{$data_array['nazev']}". " ". "{$data_array['jmeno']}";
-                //    NAZEV PREDMETU                  CELE JMENO GARANTA
-                ?>
-                <br>
-                <?php
-            }
+           
+            <form><br>
+               <input type="submit" value="Vyhledat" size="30">
+               <input type="text">
+            </form> <br>
+            
+            <table>
 
-            ?>
+                <tr bgcolor="#3d9615"><font color="#FFF">
+                    <td><font color="#FFF"><b>NAZEV PREDMETU</b></font></td> 
+        			<td><font color="#FFF"><b>GARANT PREDMETU</b></font></td>
+    			</tr>
+                   
+                    <?php
+                    $query = "SELECT * FROM predmet, vyucujici WHERE predmet.id_garant = vyucujici.id_vyucujici";
+                    $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
+                    while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){?>
+                    
+                <tr>
+                     <td> <?php echo "{$data_array['nazev']}";?> </td>                
+                     <td> <?php echo "{$data_array['jmeno']}";?> </td>  
+                </tr>    
+    
+    			<?php
+                }
+    
+                ?>
+            </table>
             
 
 <?php include("template/footer.php");?>
--- registration.php
+++ registration.php
@@ -7,25 +7,33 @@ include("template/header.php");
 ?>
 
             <h2>Registrace předmětů</h2>
+            
+            <table>
+                <tr bgcolor="#3d9615"><font color="#FFF">
+                    <td><font color="#FFF"><b>NAZEV PREDMETU</b></font></td> 
+        			<td><font color="#FFF"><b>PRIHLASENO STUDENTU</b></font></td>
+        			<td><font color="#FFF"><b>KAPACITA</b></font></td>
+        			<td><font color="#FFF"><b>REGISTRACE</b></font></td>
+    			</tr>
+            
                 <?php
                 $query = "SELECT nazev, kapacita FROM predmet";
                 $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
                 while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
-                    echo "{$data_array['nazev']}". " ";
-                    //    NAZEV PREDMETU   
+                    
+
 
                     $query = "SELECT COUNT(*) as 'pocet' FROM zapsany_predmet, predmet WHERE predmet.nazev = '{$data_array['nazev']}' AND predmet.id_predmet = zapsany_predmet.id_predmet";
                     $count_data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
-                    $count = mysqli_fetch_array($count_data, MYSQLI_ASSOC);
-                    echo "{$count['pocet']}";
-                    // PRIHLASENO  
-
-                    echo " / ";           
-
-                    echo  "{$data_array['kapacita']}";
-                    // KAPACITA
-                    ?>
-                    <br>
+                    $count = mysqli_fetch_array($count_data, MYSQLI_ASSOC);?>
+                    
+                    <tr> 
+                       <td> <?php echo "{$data_array['nazev']}";?> </td>   
+                       <td> <?php echo "{$count['pocet']}";?> </td>   
+                       <td> <?php echo "{$data_array['kapacita']}";?> </td>   
+                       <td><input type="submit" value="REGISTROVAT" size="50"></td> 
+           			</tr>  
+                    
                     <?php
                 }
 
