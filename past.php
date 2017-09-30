22b24c05bd83f16ec8cf49381be62f0d2e4a3f54
Ehmeed <milanhruban@seznam.cz>
2017-09-30 21:02:49 +0200

    vypis solo projektu

--- past.php
+++ past.php
@@ -1,5 +1,7 @@
 <?php
-	session_start();
+    session_start();
+    require_once 'check_login.php';
+    require_once 'dbconnect.php';
 ?>
 
 <!DOCTYPE html>
@@ -51,7 +53,31 @@
 
         <div class="vnitrni_obsah">
             <h2>Minulé projekty</h2>
+                <?php
+                //TODO DATE
+                $date = date("Y-m-d h:i:s");
+                $login = $_SESSION['login'];
+                $query = "SELECT projekt.nazev, informace.pocet_bodu, projekt.maximum_bodu, vyucujici.jmeno FROM student, projekt, varianta, prihlasena_varianta, informace, resitel, vyucujici WHERE student.login = '$login' AND student.id_resitel = prihlasena_varianta.id_resitel AND prihlasena_varianta.id_varianta = varianta.id_varianta AND projekt.id_projekt = varianta.projekt AND student.id_resitel = resitel.id_resitel AND resitel.info_reseni = informace.id_informace AND informace.hodnotici = vyucujici.id_vyucujici
+                ";
+                $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
+                while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
+                    // nazev projektu
+                    echo "{$data_array['nazev']} ";
 
+                    // pocet ziskanych bodu
+                    echo "{$data_array['pocet_bodu']} ";
+
+                    //maximum bodu
+                     echo "{$data_array['maximum_bodu']} ";
+
+                    //jmeno hodnoticiho
+                    echo "{$data_array['jmeno']} ";
+                    ?>
+                    <br>
+                    <?php
+                }
+
+                ?>
         </div>
 
     </div>
--- subjects.php
+++ subjects.php
@@ -53,8 +53,9 @@
 
         <div class="vnitrni_obsah">
             <h2>Zapsané předměty</h2>
-                  <?php
-                $query = "SELECT nazev FROM predmet, student, zapsany_predmet WHERE student.login = 'xhruba08' AND student.login = zapsany_predmet.login AND zapsany_predmet.id_predmet = predmet.id_predmet";
+                <?php
+                $login = $_SESSION['login'];
+                $query = "SELECT nazev FROM predmet, student, zapsany_predmet WHERE student.login = '$login' AND student.login = zapsany_predmet.login AND zapsany_predmet.id_predmet = predmet.id_predmet";
                 $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
                 while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
                     echo "{$data_array['nazev']}";
