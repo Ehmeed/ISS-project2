4d844b3b6125f5659538734f87cc5cac641eb856
David Hěl <dave.hel7@gmail.com>
2017-10-01 20:21:26 +0200

    log pro admina

--- log.php
+++ log.php
@@ -8,7 +8,14 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Fakultní informační systém</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
-    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />   
+    <script language="JavaScript" type="text/javascript">
+    function login(pop){
+    	if(pop == "popup"){
+    	    document.getElementById('login').style.visibility="visible";
+    	}
+    	}
+	</script>
 </head>
 <body>
 
@@ -20,10 +27,21 @@
             <br><h2>Fakultní informační systém</h2>
             <hr width="350px">
             <a href="index.php" style="text-decoration: none"><button class="tlacitko2"><h4>PŘIHLÁSIT SE JAKO STUDENT</h4></button></a><br><br>
-            <a href="index.php" style="text-decoration: none"><button class="tlacitko2"><h4>PŘIHLÁSIT SE JAKO VYUČUJÍCÍ</h4></button></a>
-        </center>
+            <a href="index.php" style="text-decoration: none"><button class="tlacitko2"><h4>PŘIHLÁSIT SE JAKO VYUČUJÍCÍ</h4></button></a><br><br><br><br><br>
+			<p><a href="javascript:login('popup');" style="text-decoration: none"><span class="glyphicon glyphicon-edit"></span>&nbspPřihlásit se jako administrátor</a></p>
+			
+			<div id="login"> 
+                <form name="login" action="" method="post"><br>
+                <center>Login: <input size="17" /></center><br>
+                <center>Heslo: <input size="17" /></center><br>
+                <center><input type="submit" name="submit" value="PŘIHLÁSIT SE" /></center>
+                </form>
+            </div> 
+	
+	
+	</div>
 
-    </div>
+	
 
 </div>
 
--- subjects.php
+++ subjects.php
@@ -7,18 +7,28 @@ include("template/header.php");
 ?>
 
             <h2>ZapsanÃ© pÅ™edmÄ›ty</h2>
-                <?php
-                $login = $_SESSION['login'];
-                $query = "SELECT nazev FROM predmet, student, zapsany_predmet WHERE student.login = '$login' AND student.login = zapsany_predmet.login AND zapsany_predmet.id_predmet = predmet.id_predmet";
-                $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
-                while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
-                    echo "{$data_array['nazev']}";
-                    // NAZEV PREDMETU   TODO - JESTE NECO NEBO NE?
-                    ?>
-                    <br>
+              
+            <table>
+                <tr bgcolor="#3d9615"><font color="#FFF">
+                    <td><font color="#FFF"><b>ZAPSANÉ PØEDMÌTY</b></font></td> 
+    			</tr>
+                        
+                        <?php
+                        $login = $_SESSION['login'];
+                        $query = "SELECT nazev FROM predmet, student, zapsany_predmet WHERE student.login = '$login' AND student.login = zapsany_predmet.login AND zapsany_predmet.id_predmet = predmet.id_predmet";
+                        $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
+                        while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){ ?>
+                                             
+                 <tr> 
+                  	 <td><?php echo "{$data_array['nazev']}"; ?></td>
+                 </tr> 
+  
+
                     <?php
                 }
 
                 ?>
+                
+             </table>      
                 
 <?php include("template/footer.php");?>
