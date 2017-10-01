30c92c756af0c8178d9c21e275ec16d61a1d7d27
Ehmeed <milanhruban@seznam.cz>
2017-10-01 18:29:08 +0200

    registrace predmetu komplet biiiiiiitach

--- registration.php
+++ registration.php
@@ -11,8 +11,20 @@ $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_err
 while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
   $subject = $data_array['id_predmet'];
   if (isset($_POST[$subject])){     
-    //TODO check jestli predmet neni full
-    dbqueryinsert("INSERT INTO zapsany_predmet(login, id_predmet) VALUES('$login', {$data_array['id_predmet']})", $conn);    
+    
+    $registered = mysqli_query($conn, "SELECT * FROM zapsany_predmet WHERE login='$login' AND id_predmet='$subject'");
+    if(mysqli_num_rows($registered) === 0){
+      //registrovat
+      //kontrola zda predmet neni plny
+      $registered_students = mysqli_query($conn, "SELECT * FROM zapsany_predmet WHERE id_predmet='$subject'");
+      $max_students = mysqli_query($conn, "SELECT kapacita FROM predmet WHERE id_predmet='$subject'");
+      if(mysqli_num_rows($registered_students) < mysqli_num_rows($max_students)){
+        dbqueryinsert("INSERT INTO zapsany_predmet(login, id_predmet) VALUES('$login', {$data_array['id_predmet']})", $conn);
+      }
+    }else {
+      //odhlasit
+      dbqueryinsert("DELETE FROM zapsany_predmet WHERE login = '$login' AND id_predmet = '$subject'", $conn);
+    }    
   }
 }
 
@@ -35,10 +47,18 @@ while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
                 while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
                     
 
-
+                    //pocet zaregistrovanych
                     $query = "SELECT COUNT(*) as 'pocet', predmet.id_predmet FROM zapsany_predmet, predmet WHERE predmet.nazev = '{$data_array['nazev']}' AND predmet.id_predmet = zapsany_predmet.id_predmet";
                     $count_data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
                     $count = mysqli_fetch_array($count_data, MYSQLI_ASSOC);
+
+                    mysqli_free_result($count_data);
+                    $registered = mysqli_query($conn, "SELECT * FROM zapsany_predmet WHERE login='$login' AND id_predmet='{$data_array['id_predmet']}'");
+                    if(mysqli_num_rows($registered) === 0){
+                      $button_text = "REGISTROVAT";
+                    }else {
+                      $button_text = "ODHLÁSIT";
+                    }
 
                     ?>                 
 
@@ -47,7 +67,7 @@ while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
                        <td> <?php echo "{$count['pocet']}";?> </td>   
                        <td> <?php echo "{$data_array['kapacita']}";?> </td>  
                        
-                       <td><input type="submit" value="REGISTROVAT" size="50" name="<?php echo "{$data_array['id_predmet']}"?>"></td> 
+                       <td><input type="submit" value="<?php echo $button_text ?>" size="50" name="<?php echo "{$data_array['id_predmet']}"?>"></td> 
                        
            			</tr>  
                  
