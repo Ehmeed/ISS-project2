95a543d8617217eef63f761ffca57aae321bae04
David Hěl <dave.hel7@gmail.com>
2017-10-04 01:55:02 +0200

    změny

---
+++ teacher/home.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    require_once '../check_login.php';
+    require_once '../dbconnect.php';
+    $title = 'Úvod - Fakultní informační systém';
+    include("teacher_header.php"); 
+?>
+
+<!--Obsah stranky-->          
+            <h2>Úvod</h2>
+            <h4><b>Poslední přihlášení:</b> <i><span id="currTime"></span></i></h4>
+            <h4><b>Jméno:</b> <?php 
+                    $login = $_SESSION['login'];
+                    $data_array = dbquery("SELECT jmeno FROM vyucujici WHERE login='$login'", $conn);?>
+                    <i><?php echo $data_array['jmeno']; ?></i>
+            </h4>
+            <h4><b>Login:</b> <i><?php echo $_SESSION['login'] ?> </i></h4>
+            
+        
+
+<script>
+    var time = new Date();
+    document.getElementById("currTime").innerHTML = time.toLocaleTimeString();
+</script>    
+            
+
+<?php include("teacher_footer.php");?>
---
+++ teacher/subjects.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    require_once '../check_login.php';
+    require_once '../dbconnect.php';
+    $title = 'Předměty - Fakultní informační systém';
+    include("teacher_header.php"); 
+?>
+
+<!--Obsah stranky-->          
+            <h2>Předměty</h2>
+
+            
+        
+
+<?php include("teacher_footer.php");?>
