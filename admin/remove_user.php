ce1b667e3a06069ecf6dbba2728f7d867108cf41
David Hěl <dave.hel7@gmail.com>
2017-10-04 13:57:13 +0200

    remove

--- admin/remove_user.php
+++ admin/remove_user.php
@@ -28,11 +28,30 @@
 <!--Obsah stranky-->          
             <h2>Odebrat uživatele</h2>
                
-				<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off"><br>
+				<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
                 <br><div class="msg"><?php echo "{$message}";?></div>
 					   <input id="box" type="text" name="search">
 					   <input type="submit" value="Vyhledat uživatele" size="30" name="submit">
 				</form>
+				
+				<br><table>
+
+					<tr bgcolor="#3d9615"><font color="#FFF">
+						<td><font color="#FFF"><b>JMÉNO</b></font></td> 
+						<td><font color="#FFF"><b>LOGIN</b></font></td>
+						<td><font color="#FFF"><b>ZAŘAZENÍ</b></font></td>
+						<td><font color="#FFF"><b>ODEBRAT</b></font></td>
+					</tr>
+					   
+				 
+					<tr>
+						<td> X </td>                
+						<td> X </td>  
+						<td> X </td>  
+						<td><input type="submit" value="<?php ?>"></td> 
+					</tr>    
+
+            </table>
             
          
 
