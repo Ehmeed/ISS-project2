0f6d3b655e24a8cd93c13aa61d46e9fdb266dc8e
Ehmeed <milanhruban@seznam.cz>
2017-10-05 10:40:07 +0200

    team_projects funkcni

--- past.php
+++ past.php
@@ -12,23 +12,23 @@ include("template/header.php");
             <table>
                 <tr bgcolor="#3d9615"><font color="#FFF">
                     <td><font color="#FFF"><b>PROJEKT</b></font></td>
+                    <td><font color="#FFF"><b>TEAM</b></font></td> 
 					<td><font color="#FFF"><b>PŘEDMĚT</b></font></td> 
 					<td><font color="#FFF"><b>HODNOTÍCÍ</b></font></td> 
-					<td><font color="#FFF"><b>ZÍSKANÝCH BODŮ</b></font></td> 
+					<td><font color="#FFF"><b>BODŮ</b></font></td> 
 					<td><font color="#FFF"><b>MAX. BODŮ</b></font></td> 
 					<td><font color="#FFF"><b>DEADLINE</b></font></td> 
     			</tr>
 				
 				<?php
-                //TODO DATE, TEAM PROJECT
-                $date = date("Y-m-d h:i:s");
+                //$date = date("Y-m-d h:i:s");
                 $login = $_SESSION['login'];
                 $query = "SELECT DISTINCT projekt.nazev, informace.pocet_bodu, projekt.maximum_bodu, vyucujici.jmeno, predmet.nazev, projekt.datum_odevzdani FROM student, projekt, varianta, prihlasena_varianta, informace, vyucujici, predmet WHERE student.login = '$login' AND student.id_resitel = prihlasena_varianta.id_resitel AND prihlasena_varianta.id_varianta = varianta.id_varianta AND projekt.id_projekt = varianta.projekt AND prihlasena_varianta.info_reseni = informace.id_informace AND informace.hodnotici = vyucujici.id_vyucujici AND projekt.predmet = predmet.id_predmet AND projekt.datum_odevzdani < now()
                 ";?>
                 
 				<!--  JEDNOTLIVCI -->
 				<tr bgcolor="#656665">
-						<td colspan="6"><font color="#FFF"><b>SAMOSTATNÉ</b></font></td> 
+						<td colspan="7"><font color="#FFF"><b>SAMOSTATNÉ</b></font></td> 
 					</tr>    
 				<tr>
 				
@@ -37,18 +37,46 @@ include("template/header.php");
                 while($data_array = mysqli_fetch_array($data, MYSQLI_NUM)){ ?>
 	
 				    <td><?php echo "{$data_array[0]} ";?></td> 
+				    <td><?php echo "";?></td> 
                     <td><?php echo "{$data_array[4]} ";?></td>
                     <td><?php echo "{$data_array[3]} ";?></td>
                     <td><?php echo "{$data_array[1]} ";?></td>
 					<td><?php echo "{$data_array[2]} ";?></td>
 					<td><?php echo "{$data_array[5]} ";?></td>
 			   </tr>
-				<?php } ?>
+				<?php } 
+					//teamy
+					$query = "SELECT DISTINCT projekt.nazev, tym.nazev_tymu, informace.pocet_bodu, projekt.maximum_bodu, vyucujici.jmeno, predmet.nazev, projekt.datum_odevzdani FROM student, projekt, varianta, prihlasena_varianta, informace, vyucujici, predmet, tym, clenove_teamu WHERE 
+						student.login = '$login' AND
+						student.login = clenove_teamu.login_clena AND
+						clenove_teamu.id_teamu = tym.id_resitel AND
+						tym.id_resitel = prihlasena_varianta.id_resitel AND
+						prihlasena_varianta.id_varianta = varianta.id_varianta AND
+						projekt.id_projekt = varianta.projekt AND
+						prihlasena_varianta.info_reseni = informace.id_informace AND
+						informace.hodnotici = vyucujici.id_vyucujici AND
+						projekt.predmet = predmet.id_predmet AND
+						projekt.datum_odevzdani < now()";
+				?>
 				
 				<!--  TÝMY -->
 				<tr bgcolor="#656665">
-						<td colspan="6"><font color="#FFF"><b>TÝMOVÉ</b></font></td> 
-				</tr>    
+						<td colspan="7"><font color="#FFF"><b>TÝMOVÉ</b></font></td> 
+				</tr>  
+
+				<?php $data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
+
+                while($data_array = mysqli_fetch_array($data, MYSQLI_NUM)){ ?>
+	
+				    <td><?php echo "{$data_array[0]} ";?></td> 
+				    <td><?php echo "{$data_array[1]} ";?></td> 
+                    <td><?php echo "{$data_array[5]} ";?></td>
+                    <td><?php echo "{$data_array[4]} ";?></td>
+                    <td><?php echo "{$data_array[2]} ";?></td>
+					<td><?php echo "{$data_array[3]} ";?></td>
+					<td><?php echo "{$data_array[6]} ";?></td>
+			   </tr>
+				<?php } ?>  
 				
              </table> 
 			</div>	
