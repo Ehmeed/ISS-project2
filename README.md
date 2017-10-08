9feb6b40d79dc7aa8decd2e1f1085b3560156e4e
Ehmeed <milanhruban@seznam.cz>
2017-10-08 16:48:24 +0200

    prihlaseni jednotlivce done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ISS-project2
+
+ jeste mi zvyva dodelar prihlasovani na projekty, 
+ odhalsovani, 
+ zobrazeni info o reseni 
+ a odevzdani reseni, 
+ a odstranovani clenu teamu a toj e vse
--- project.php
+++ project.php
@@ -8,7 +8,7 @@ if(!isset($id) or empty($id)){
 	header("Location: home.php");
     exit;
 }
-
+$my_id = mysqli_fetch_array(mysqli_query($conn, "SELECT id_resitel FROM student WHERE login = '$login'"), MYSQLI_NUM)[0];
 //nacteni dat projektu
 $data = mysqli_query($conn,"SELECT DISTINCT projekt.nazev, projekt.popis, projekt.maximum_bodu, projekt.minimum_bodu, projekt.datum_odevzdani, vyucujici.jmeno, predmet.nazev, projekt.datum_prihlaseni FROM projekt, predmet, vyucujici WHERE projekt.id_projekt = $id AND projekt.predmet = predmet.id_predmet AND projekt.zadavatel = vyucujici.id_vyucujici") or die("Cannot access database.").mysqli_error($conn);
 $data_array = mysqli_fetch_array($data, MYSQLI_NUM);
@@ -16,9 +16,6 @@ if(mysqli_num_rows($data) != 1){
 	header("Location: home.php");
     exit;
 }
-
-// ZDE ZPRACOVANI PRI KLIKNUTI NA TLACITKO PRIHLASIT
-
 
 $title = $data_array[0];
 $desc = $data_array[1];
@@ -29,6 +26,60 @@ $vyucujici = $data_array[5];
 $predmet = $data_array[6];
 $datum_registrace = $data_array[7];
 $button_text = '';
+$message = '';
+$team_id = '';
+
+// ZDE ZPRACOVANI PRI KLIKNUTI NA TLACITKO PRIHLASIT
+$query = "SELECT varianta.id_varianta FROM varianta";
+$data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
+while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){
+  $variant = $data_array['id_varianta'];
+  if (isset($_POST[$variant])){
+  	//kontrola zda jiz student neni prihalseny
+	$team_id = $_POST['tymy'];
+	//TODO CHECK DATe
+	if($team_id == "0"){
+		//jednotlivec
+		$errors = mysqli_num_rows(mysqli_query($conn, "SELECT DISTINCT * FROM prihlasena_varianta, student, projekt, varianta WHERE student.login = '$login' AND prihlasena_varianta.id_resitel = student.id_resitel AND prihlasena_varianta.id_varianta = varianta.id_varianta AND varianta.projekt = projekt.id_projekt AND projekt.id_projekt = $id"));
+		$reged_as_single = $errors;
+		$errors = $errors + mysqli_num_rows(mysqli_query($conn, "SELECT DISTINCT * FROM clenove_teamu, prihlasena_varianta, varianta, projekt WHERE clenove_teamu.login_clena = '$login' AND prihlasena_varianta.id_resitel = clenove_teamu.id_teamu AND prihlasena_varianta.id_varianta = varianta.id_varianta AND varianta.projekt = projekt.id_projekt AND projekt.id_projekt = $id"));
+
+		if($errors != 0){
+			$message = 'Na tento projekt jste již přihlášen';
+		}
+		$reged = mysqli_num_rows(mysqli_query($conn,"SELECT DISTINCT predmet.id_predmet FROM zapsany_predmet, projekt, predmet WHERE
+			zapsany_predmet.login = '$login' AND zapsany_predmet.id_predmet = predmet.id_predmet AND projekt.predmet = predmet.id_predmet AND projekt.id_projekt = $id"));
+		if($reged != 1){
+			$message = 'Na tento projekt se nemůžete přihlásit, nemáte zapsaný odpovídající předmět.';
+		}
+
+		if($errors == 0 and $reged == 1){
+			$prihlasenych = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM prihlasena_varianta WHERE id_varianta = $variant"));
+			$kapacita = mysqli_fetch_array(mysqli_query($conn, "SELECT maximum_resitelu FROM varianta WHERE id_varianta = $variant"), MYSQLI_NUM)[0];
+			if($kapacita <= $prihlasenych){
+				$message = 'Varianta je již plně obsazená';
+			}else {
+				if(mysqli_query($conn, "INSERT INTO prihlasena_varianta(id_resitel, id_varianta) VALUES($my_id, $variant)")){
+					$message = 'Byl jste přihlášen';
+				}else {
+					$message = 'Přihlášení se nezdařilo';
+				}
+			}
+		}else if($reged_as_single == 1 and $_POST[$variant] = 'ODHLÁSIT'){
+			mysqli_query($conn, "DELETE FROM prihlasena_varianta WHERE id_resitel = $my_id AND id_varianta = $variant");
+			$message = 'Byl jste odhlášen';
+		}
+
+	}else {
+		//team
+		$team_id = substr($team_id, 5);
+	}
+  	break;
+  }
+} 
+
+
+
 
 include("template/header.php");
 ?>
@@ -41,12 +92,12 @@ include("template/header.php");
 
 					<?php
                 	$query = "SELECT tym.id_resitel, tym.nazev_tymu FROM tym, clenove_teamu WHERE
-                	 login_clena = '$login' AND clenove_teamu.id_teamu = tym.id_resitel";
+                	 login_clena = '$login' AND clenove_teamu.id_teamu = tym.id_resitel AND tym.login_vedouciho = '$login'";
                 	$data = mysqli_query($conn, $query) or die("Cannot access database.").mysqli_error($conn);
                		while($data_array = mysqli_fetch_array($data, MYSQLI_ASSOC)){ 	
                		?>
 
-               		<option value="<?php echo "team:".$data_array['id_resitel']?>"><?php echo $data_array['nazev_tymu']?></option> 
+               		<option value="<?php echo "team:".$data_array['id_resitel']?>" <?php  if($team_id==$data_array['id_resitel']) echo "selected"?>><?php echo $data_array['nazev_tymu']?></option> 
 
                		<?php
                		}
@@ -65,6 +116,7 @@ include("template/header.php");
 			<h2>Varianty</h2>
 			<div id="table-scroll">
 			 <br> 			
+			<div class="msg"><?php echo "{$message}";?></div>
             <table>
                 <tr bgcolor="#3d9615"><font color="#FFF">
                     <td><font color="#FFF"><b>VARIANTA</b></font></td> 
@@ -103,7 +155,7 @@ include("template/header.php");
                   	 <td><a href="<?php echo $link.$data_array['id_varianta']?>"><?php echo $data_array['id_varianta']?></a></td>	
 					 <td><?php echo $count[0]?></td>
 					 <td><?php echo $data_array['maximum_resitelu']?></td>
-					 <td><input type="submit" name="<?php echo $data_array['id_varianta']?>"value="<?php echo $button_text ?>"></td><!-- TADY MÁŠ TEN BUTTON -->
+					 <td><input type="submit" name="<?php echo $data_array['id_varianta']?>"value="<?php echo $button_text ?>"></td>
                  </tr> 
                  <?php
                  }
