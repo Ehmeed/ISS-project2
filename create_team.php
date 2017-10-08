313d57f25388603fbe3238c5a0fe39176c894981
Ehmeed <milanhruban@seznam.cz>
2017-10-08 13:44:06 +0200

    create team do

--- create_team.php
+++ create_team.php
@@ -4,12 +4,45 @@ require_once 'check_login.php';
 require_once 'dbconnect.php';
 $title = 'Vytvořit tým - Fakultní informační systém';
 include("template/header.php");
+
+$login = $_SESSION['login'];
+$message = '';
+$name = '';
+$nameError = '';
+
+if(isset($_POST['pridat'])){
+	$jmeno = htmlspecialchars(strip_tags(trim($_POST['jmeno'])));
+
+	if(empty($jmeno) or strlen($jmeno) >= 30){
+		$nameError = 'Název teamu musí mít 1-30 znaků';
+	} else {
+		if(mysqli_query($conn, "INSERT into RESITEL() VALUES()")){
+			$id = mysqli_insert_id($conn);
+			if(mysqli_query($conn, "INSERT into tym(id_resitel, nazev_tymu, login_vedouciho) values($id, '$jmeno', '$login')")){
+				
+				if(mysqli_query($conn, "INSERT into clenove_teamu(id_teamu, login_clena) VALUES($id, '$login')")){
+					$message = 'Tým vytvořen!';
+				}else {
+					$message = 'Upss, tým se nepodařilo vytvořit';
+					mysqli_query($conn, "DELETE from tym WHERE id_resitel = $id");
+					mysqli_query($conn, "DELETE from resitel WHERE id_resitel = $id");
+				}
+			}else {
+				$message = 'Upss, tým se nepodařilo vytvořit';				
+				mysqli_query($conn, "DELETE from tym WHERE id_resitel = $id");
+			}
+		}else {
+			$message = 'Upss, tým se nepodařilo vytvořit';
+		}
+	}
+
+}
 ?>
 
      	<h2>Vytvořit tým</h2><br>	
 		
 			<div class="formular">
-				<form method="post">
+				<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off"><br> 
 				<div class="msg"><?php echo "{$message}";?></div>
 					<h4>*Název</h4>
 						<input id="box" type="text" name="jmeno" value="">
